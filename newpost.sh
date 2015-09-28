f7acf8d7b91150907eacfb4a627a719f30e87c80
daigo <dragonmind2002@yahoo.co.jp>
2015-09-29 00:14:37 +0900

    rebuilding site 2015年 9月29日 火曜日 00時14分37秒 JST

--- newpost.sh
+++ newpost.sh
@@ -3,4 +3,4 @@
 title=`echo $1 | tr "A-Z" "a-z"`
 title=`echo $title | tr " " "-"`
 title=`echo $title | sed "s/\.//g"`
-hugo new post/"`date +%Y-%m-%d`-$title.markdown"
+hugo new post/"$title.markdown"
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf public/*
+#hugo server -t greyshade --watch
+hugo server -t beg --watch
